6bf41783d1e3262a7df0d99a84a4522daf852c76
Sergey Rachev <sergey.rachev@gmail.com>
2020-04-18 00:38:34 +0200

    - removed project specific location

--- cmake/export.cmake
+++ cmake/export.cmake
@@ -8,6 +8,4 @@ set(${API_NAME}_SHARED_LIBRARY ${BUILD_SHARED_LIBS})
 set(CMAKE_DEFINE "#cmakedefine")
 
 configure_file(${CMAKE_CURRENT_LIST_DIR}/../res/export_configuration.h.in.in ${CMAKE_CURRENT_BINARY_DIR}/res/export_configuration.h.in @ONLY)
-configure_file(${CMAKE_CURRENT_BINARY_DIR}/res/export_configuration.h.in ${CMAKE_CURRENT_BINARY_DIR}/neuon/export_configuration.h @ONLY)
-
 cmake_policy(POP)
