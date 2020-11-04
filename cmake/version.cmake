b27b208e1418168c120f8dbd0ea77c43f361aa19
Sergey Rachev <sergey.rachev@gmail.com>
2020-11-04 23:56:32 +0100

    - put generated files to subfolder to avoid collision on multiple usage

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -12,10 +12,11 @@ function(configure_version symbols_space_name)
 
     set(version_revision ${version}+g${revision} CACHE INTERNAL "Version Revision string")
 
-    configure_file(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../res/version.h.in ${CMAKE_CURRENT_BINARY_DIR}/version.h @ONLY)
-    configure_file(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../res/version.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/version.cpp @ONLY)
+    configure_file(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../res/version.h.in ${CMAKE_CURRENT_BINARY_DIR}/generated/version.h @ONLY)
+    configure_file(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../res/version.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/generated/version.cpp @ONLY)
 
     message(STATUS "You are able to use fully qualified variable version_revision to refer to version and revision")
+    message(STATUS "Don't forget to add CMAKE_CURRENT_BINARY_DIR/generated (which is ${CMAKE_CURRENT_BINARY_DIR}/generated) to your target include directories with PRIVATE BUILD_INTERFACE")
 endfunction()
 
 function(configure_birthday symbols_space_name)
@@ -26,8 +27,9 @@ function(configure_birthday symbols_space_name)
     string(TIMESTAMP birthday_string UTC)
     set(birthday ${birthday_string} CACHE INTERNAL "The date of birth")
 
-    configure_file(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../res/birthday.h.in ${CMAKE_CURRENT_BINARY_DIR}/birthday.h)
-    configure_file(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../res/birthday.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/birthday.cpp)
+    configure_file(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../res/birthday.h.in ${CMAKE_CURRENT_BINARY_DIR}/generated/birthday.h)
+    configure_file(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../res/birthday.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/generated/birthday.cpp)
 
     message(STATUS "You are able to use variable birthday to refer to build date")
+    message(STATUS "Don't forget to add CMAKE_CURRENT_BINARY_DIR/generated (which is ${CMAKE_CURRENT_BINARY_DIR}/generated) to your target include directories with PRIVATE BUILD_INTERFACE")
 endfunction()
